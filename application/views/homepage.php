fdb44699ea9cc97a56d26ea8299c806e7036f0eb
harysusilo50 <harysusilo50@gmail.com>
2021-06-04 03:14:44 +0700

    menghapus source gambar logo error

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -1,7 +1,7 @@
 <body style="font-family:poppins; background-color: black">
     <nav class="navbar sticky-top navbar-expand-lg navbar-dark shadow-lg" style="background-color: black;">
         <div class="container">
-            <a class="navbar-brand" href="<?= base_url('dashboard') ?>"><img src="https://static-assets.profiles.hallyulife.com/izone-logo-fix.png" width="100px"></a>
+            <a class="navbar-brand" href="<?= base_url('dashboard') ?>"><img src="" width="100px"></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
